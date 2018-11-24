5d4ee351454ec4e88af373d2cb88aad684c0ca60
Brett Ashley McCoubrey <brettashley@Bretts-MacBook.local>
2018-11-24 12:36:31 -0800

    add get_artist_song function

--- src/scraper.py
+++ src/scraper.py
@@ -1,34 +1,34 @@
-
 import selenium
 from selenium.webdriver import Firefox, Chrome
 import time
 
 class Scraper():
 
-    __init__(self, browser='Firefox()', url):
-    self.b = browser
-    self.start_url = url
-    b.get(url)
+    def __init__(self, url, browser=Firefox()):
+        self.b = browser
+        self.start_url = url
+        self.b.get(self.start_url)
 
 
     def _accept_cookies(self):
-    try:
-        button = self.b.find_element_by_css_selector('button.qc-cmp-button')
-        button.click()
-    except selenium.common.exceptions.NoSuchElementException:
-        pass
+        try:
+            button = self.b.find_element_by_css_selector('button.qc-cmp-button')
+            button.click()
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
 
 
         # desired_section = 'Most influential artists'
         # sel = "div#content div.divided-layout div.layout-container.leftContent div"
 
-    def find_desired_section(self, sect_divs, desired_section):
+    def find_desired_section(self, sel, desired_section):
+        self._accept_cookies()
+        sect_divs = self.b.find_elements_by_css_selector(sel)
         for i, div in enumerate(sect_divs):
             if div.text.startswith(desired_section):
                 return sect_divs[i+1]
 
-    def get_artist_urls(self, sect_divs, desired_section):
-        artist_section = self.find_desired_section(sect_divs, desired_section)
+    def get_artist_urls(self, artists_section):
         artist_tiles = artists_section.find_elements_by_css_selector('li')
         artists = {}
         for tile in artist_tiles:
@@ -39,3 +39,14 @@ class Scraper():
             yield {'name': artist_name,
                     'url': a.get_attribute('href')}
 
+
+    def get_artist_songs(self, artist_url_dict):
+        self._accept_cookies()
+        sel = "div#content div.artistContent"
+        section_divs = self.b.find_element_by_css_selector(sel)
+        tracks = section_divs.find_elements_by_css_selector('h3.trackName')
+        track_urls = {}
+        for track in tracks:
+            a = track.find_element_by_css_selector('a')
+            yield {'track_name': track.text,
+                   'url': a.get_attribute('href')}
