38285545ae55f3f02182e4e787becbfc00b03118
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-01 21:41:59 -0800

    add ability to scrape songs without artist id

--- src/scrape.py
+++ src/scrape.py
@@ -5,6 +5,7 @@ from selenium.webdriver import Firefox, Chrome
 import time
 from scraper import Scraper
 from DatabaseInteraction import DatabaseInteraction
+import numpy as np
 
 url_soul_funk_disco = 'https://www.whosampled.com/genre/Soul-Funk-Disco/'
 
@@ -16,7 +17,7 @@ def main(url=None, get_genre=True, get_first_artist_songs=True, artists_to_scrap
 
     if get_genre:
 
-        desired_section = 'Most influential artists'
+        desired_section = 'Most popular artists'
         sel = "div#content div.divided-layout div.layout-container.leftContent div"
 
         # scrape and write artists to DB
@@ -24,9 +25,12 @@ def main(url=None, get_genre=True, get_first_artist_songs=True, artists_to_scrap
         db.write_artists(artists)
 
         artists_to_scrape = []
+        scraped_status = []
         for artist in artists:
             artist_dict = db.get_artist_info(url=artist["url"])
-            artists_to_scrape.append(artist_dict["id"])
+            if artist_dict["scraped"] == 0:
+                artists_to_scrape.append(artist_dict["id"])
+
 
     if artists_to_scrape is None:
         n_artists_to_scrape = 100
@@ -61,15 +65,46 @@ def main(url=None, get_genre=True, get_first_artist_songs=True, artists_to_scrap
             for song_dict in sampled_in:
                 sample_id = db.get_song_id(song_dict['url'])
                 db.insert_contains_sample(song['id'], sample_id)
+
+            for song_dict in contains:
+                sampled_id = db.get_song_id(song_dict['url'])
+                db.insert_contains_sample(sampled_id, song['id'])
         
         db.update_scraped_status('artists', artist['id'], 1)
         counter += 1
 
+def scrape_songs():
+    db = DatabaseInteraction()
+    s = Scraper()
+    while db.get_next_song_to_scrape():
+        song = db.get_next_song_to_scrape()
+        # print(song['url'])
+        sampled_in, contains, new_artists = s.get_song_connections(song['url'])
+        db.write_artists(new_artists)
+        db.write_songs(sampled_in)
+        db.write_songs(contains)
+        db.update_scraped_status('songs', song['id'], 1)
+
+        for song_dict in sampled_in:
+            sample_id = db.get_song_id(song_dict['url'])
+            db.insert_contains_sample(song['id'], sample_id)
+
+        for song_dict in contains:
+            sampled_id = db.get_song_id(song_dict['url'])
+            db.insert_contains_sample(sampled_id, song['id'])
+
+
+
+
+
 # artists_to_scrape = [485, 489, 1749, 60, 476, 350, 3351]
 # main(get_genre=False, get_first_artist_songs=False, artists_to_scrape=artists_to_scrape)
+# main(url='https://www.whosampled.com/genre/Electronic-Dance/')
+# main(url='https://www.whosampled.com/genre/Hip-Hop/',
+#      get_genre=True,
+#      get_first_artist_songs=False)
 
-main(url='https://www.whosampled.com/genre/Electronic-Dance/')
-
+scrape_songs()
 
 
 
