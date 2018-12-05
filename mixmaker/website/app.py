b179e65ec8092dc613e2781664bec6ebc659827a
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-05 14:53:20 -0800

    add table variables and db functions

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -1,5 +1,7 @@
 import pickle
+import pandas as pd
 from flask import Flask, request, render_template, jsonify
+from ..database_interaction import DatabaseInteraction
 
 
 # with open('.pkl') as f:
@@ -7,8 +9,17 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__, static_url_path="")
 
+
+db = DatabaseInteraction()
+
 @app.route('/')
 def index():
     """Return the main page."""
-    ajj='stupendous'
-    return render_template('index.html', ajj=ajj)
+    search = 'search'
+    all_songs = db.get_song_and_artist_names()
+    artist_names = pd.DataFrame(all_songs['artist_name'].unique(),
+                     columns=['artist_name'])
+    return render_template('index.html',
+                                search=search,
+                                all_songs=all_songs,
+                                artist_names=artist_names)
