1f5f51934e2f316dcd16456431687d7f371d181a
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-05 18:43:15 -0800

    function testing

---
+++ mixmaker/web_functions.py
@@ -0,0 +1,14 @@
+import pandas as import pd
+import numpy as np
+from database_interaction import DatabaseInteraction
+
+def load_songs(db_name='mixmaker', host='localhost'):
+    songs = 
+
+
+
+
+
+
+
+
