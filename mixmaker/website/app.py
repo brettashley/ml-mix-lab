a38af83bee11dc7cd025934e0bfe9a972e989396
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-06 15:57:39 -0800

    add songs and artist selectors

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
-from ..database_interaction import DatabaseInteraction
+from ..web_functions import WebFunctionHandler
 
 
 # with open('.pkl') as f:
@@ -9,33 +9,31 @@ from ..database_interaction import DatabaseInteraction
 
 app = Flask(__name__, static_url_path="")
 
-
-db = DatabaseInteraction()
+wfh = WebFunctionHandler()
 
 @app.route('/')
 def index():
     """Return the main page."""
-    all_songs = db.get_song_and_artist_names()
-    all_songs_dict = all_songs.to_dict()
-    artist_names = pd.DataFrame(all_songs['artist_name'].unique(),
-                     columns=['artist_name'])
-    artist_names_data = artist_names[:10].to_html()
-    
-    artist_songs = ''
+    # all_songs = db.get_song_and_artist_names()
+    artist_names = wfh.get_unique_artist_names()[:10].to_html()
+    artists_selection = wfh.get_artist_selections(100)
     return render_template('index.html',
-                                artist_names=artist_names_data,
-                                artist_songs=artist_songs)
+                                artist_names=artist_names,
+                                artists_selection=artists_selection)
 
-# @app.route('/<artist>')
-# def artist_songs(artist):
-#     """Return songs for an artist."""
-#     artist_songs =  db.get_artist_songs(artist) # write to take in artist name
-#     return render_template('index.html',
-#                                 artist_songs=artist_songs)
+@app.route('/artist/<int:artist_id>')
+def artist_songs(artist_id):
+    """Return songs for an artist."""
+    artist_songs = wfh.get_selector_for_songs(artist_id)
+    return render_template('index.html',
+                                artist_songs=artist_songs)
     
+
+
 
 # @app.route('/recommendations/<id>')   
 # def recommendations(id):
 #     recs = db.get_recommendations(id)
 #     return render_template('index.html',
 #                                 recs=recs)
+
