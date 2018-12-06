8eda5efc1b42aa138f8a7e81b6641c67d5a673e2
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-06 15:58:03 -0800

    functions to render selectors as html

--- mixmaker/web_functions.py
+++ mixmaker/web_functions.py
@@ -1,10 +1,51 @@
-import pandas as import pd
+
+import pandas as pd
 import numpy as np
-from database_interaction import DatabaseInteraction
+from .database_interaction import DatabaseInteraction
 
-def load_songs(db_name='mixmaker', host='localhost'):
-    songs = 
+db = DatabaseInteraction()
+class WebFunctionHandler():
 
+    def __init__(self):
+        self.db = DatabaseInteraction()
+
+    def filter_songs_by_artist(self, df, id):
+        return df.loc[:,'corrected_artist_id']
+
+    def get_unique_artist_names(self):
+        return self.db.get_artist_names()
+
+    def get_songs_for_artist(self, artist_id):
+        return (self.db.get_song_and_artist_names(artist_id=artist_id)
+                    .to_html())
+    
+    def get_artist_selections(self, n_artists):
+        artists = self.db.get_artist_names()
+        artists = artists.sort_values(by='id').set_index('id')
+        output = '<select name="artists" size="5">'
+        for i, artist in artists.iterrows():
+            if i < n_artists:
+                artist_id = i
+                artist_name = artist[0]
+                output += f'\n    <option value="{artist_id}">{artist_name}</option>'
+            else:
+                break
+        return output + '\n</select>'
+
+    def get_selector_for_songs(self, artist_id):
+        songs_df = self.db.get_song_and_artist_names(artist_id=artist_id)
+        songs_df = songs_df.set_index('id')
+        output = '<select name="songs" size="5">'
+        for i, song in songs_df.iterrows():
+                song_id = i
+                song_name = song[1]
+                output += f'\n    <option value="{song_id}">{song_name}</option>'
+        return output + '\n</select>'
+
+    
+        
+
+    
 
 
 
