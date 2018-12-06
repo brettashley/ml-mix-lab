0c5a1317bdaadd9cf04a877ebd69287ff65c1f25
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-05 18:41:29 -0800

    initial structure for app.py

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -19,7 +19,19 @@ def index():
     all_songs = db.get_song_and_artist_names()
     artist_names = pd.DataFrame(all_songs['artist_name'].unique(),
                      columns=['artist_name'])
+    
+    artist_songs = pd.DataFrame(filtered_songs, columns=['song_name'])
     return render_template('index.html',
                                 search=search,
                                 all_songs=all_songs,
-                                artist_names=artist_names)
+                                artist_names=artist_names,
+                                artist_songs=artist_songs)
+
+@app.route('/<artist>')
+def artist_songs(artist):
+    return  db.get_artist_songs(artist) # write to take in artist name
+    
+
+@app.route('/recommendations/<id>')   
+def recommendations(id):
+    return db.get_recommendations_for_song(song_id)
