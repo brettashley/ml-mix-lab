df3b484773abbe48f654aa4746e6c632ca9d1fef
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-06 11:37:00 -0800

    show dfs in html

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -15,23 +15,27 @@ db = DatabaseInteraction()
 @app.route('/')
 def index():
     """Return the main page."""
-    search = 'search'
     all_songs = db.get_song_and_artist_names()
+    all_songs_dict = all_songs.to_dict()
     artist_names = pd.DataFrame(all_songs['artist_name'].unique(),
                      columns=['artist_name'])
+    artist_names_data = artist_names[:10].to_html()
     
-    artist_songs = pd.DataFrame(filtered_songs, columns=['song_name'])
+    artist_songs = ''
     return render_template('index.html',
-                                search=search,
-                                all_songs=all_songs,
-                                artist_names=artist_names,
+                                artist_names=artist_names_data,
                                 artist_songs=artist_songs)
 
-@app.route('/<artist>')
-def artist_songs(artist):
-    return  db.get_artist_songs(artist) # write to take in artist name
+# @app.route('/<artist>')
+# def artist_songs(artist):
+#     """Return songs for an artist."""
+#     artist_songs =  db.get_artist_songs(artist) # write to take in artist name
+#     return render_template('index.html',
+#                                 artist_songs=artist_songs)
     
 
-@app.route('/recommendations/<id>')   
-def recommendations(id):
-    return db.get_recommendations_for_song(song_id)
+# @app.route('/recommendations/<id>')   
+# def recommendations(id):
+#     recs = db.get_recommendations(id)
+#     return render_template('index.html',
+#                                 recs=recs)
