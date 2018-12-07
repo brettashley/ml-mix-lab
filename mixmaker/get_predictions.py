7984fca531022ebb95311efe42ecd47177e907f4
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-07 12:29:55 -0800

    function to get predictions for all users and return pandas df

--- mixmaker/get_predictions.py
+++ mixmaker/get_predictions.py
@@ -3,6 +3,25 @@ from database_interaction import DatabaseInteraction
 from model import SongRecommender
 
 
+sr = SongRecommender(itemCol='song_id',
+                     userCol='sampled_by_song_id',
+                     ratingCol='is_connected',
+                     nonnegative=True,
+                     regParam=0.01,
+                     rank=10)
+
+db = DatabaseInteraction(db_name=mixmaker)
+
+df = db.get_table('connections')
+data = sr.spark.createDataFrame(df)
+recommender = sr.fit(data)
+
+
+
+
+
+
+
 
 
 
