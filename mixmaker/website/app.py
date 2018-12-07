f663eb5dd72ceb35b90b9082ba8dd41f89ca206a
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-07 15:11:58 -0800

    get table to appear on site

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -21,32 +21,22 @@ def index():
                                 artist_names=artist_names,
                                 artists_selection=artists_selection)
 
-@app.route('/artist/<int:artist_id>')
-def artist_songs(artist_id):
-    """Return songs for an artist."""
-    artist_songs = wfh.get_selector_for_songs(artist_id)
-    return render_template('index.html',
-                                artist_songs=artist_songs)
-    
-# @app.route('/recommendations/<int:song_id>')
-# def recommendations(song_id):
-#     """Return recommendations for a song."""
-#     recommendations = wfh.get_recommendations_for_a_song(song_id)
-#     return render_template('index.html',
-#                                 artist_songs=artist_songs)
 
 @app.route('/get_selector_for_songs/<int:artist_id>')
 def get_selector_for_songs(artist_id):
-    return wfh.get_selector_for_songs(artist_id)
+    artist_songs = wfh.get_selector_for_songs(artist_id)
+    return artist_songs
 
 @app.route('/get_predictions/<int:song_id>')
 def get_predictions(song_id):
     return wfh.get_predictions(song_id)
 
 
-# @app.route('/recommendations/<id>')   
-# def recommendations(id):
-#     recs = db.get_recommendations(id)
+# @app.route('/artist/<int:artist_id>')
+# def artist_songs(artist_id):
+#     """Return songs for an artist."""
+#     artist_songs = wfh.get_selector_for_songs(artist_id)
+#     results = wfh.get_predictions(song_id)
 #     return render_template('index.html',
-#                                 recs=recs)
-
+#                                 artist_songs=artist_songs,
+#                                 results=results)
