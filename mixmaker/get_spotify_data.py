99020ce56e461b2b5546bfb08762cf136b5778d9
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-06 19:06:55 -0800

    search an artist

--- mixmaker/get_spotify_data.py
+++ mixmaker/get_spotify_data.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
+import spotify_scraper
+import database_interaction
 
+ss = spotify_scraper.SpotifyScraper()
+db = database_interaction.DatabaseInteraction()
+
+def get_artist(artist_id=None, artist_name=None):
+    if artist_name is None:
+        artist = db.get_next_artist_for_spotify()
+        artist_name = artist['name']
+    tracks = ss.search_artist_get_tracks(artist['name'])
