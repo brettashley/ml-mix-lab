15b5ee7564e887d0aab149743c33c82fd54634bb
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-11 10:13:38 -0800

    fix background images

--- mixmaker/website/app.py
+++ mixmaker/website/app.py
@@ -14,12 +14,8 @@ wfh = WebFunctionHandler()
 @app.route('/')
 def index():
     """Return the main page."""
-    # all_songs = db.get_song_and_artist_names()
-    artist_names = wfh.get_unique_artist_names()[:10].to_html()
-    artists_selection = wfh.get_artist_selections(100)
-    return render_template('index.html',
-                                artist_names=artist_names,
-                                artists_selection=artists_selection)
+    artists_selection = wfh.get_artist_selections(500)
+    return render_template('index.html', artists_selection=artists_selection)
 
 
 @app.route('/get_selector_for_songs/<int:artist_id>')
