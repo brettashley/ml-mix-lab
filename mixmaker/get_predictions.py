d098fb286545958744ad0d85bd5086cd88e5dd05
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-12 10:00:06 -0800

    change to implicit prefs and larger alpha

--- mixmaker/get_predictions.py
+++ mixmaker/get_predictions.py
@@ -5,20 +5,27 @@ import model
 
 def get_and_write_predictions():
     sr = model.SongRecommender(itemCol='song_id',
-                        userCol='sampled_by_song_id',
-                        ratingCol='is_connected',
-                        nonnegative=True,
-                        regParam=0.01,
-                        rank=200,
-                        alpha=1)
+                     userCol='sampled_by_song_id',
+                     ratingCol='is_connected',
+                     nonnegative=True,
+                     regParam=0.01,
+                     rank=100,
+                     alpha=30,
+                     implicitPrefs=True)
+
 
     db = database_interaction.DatabaseInteraction(db_name='mixmaker')
-
+    
+    print('Getting data...')
     df = db.get_table('connections')
     data = sr.spark.createDataFrame(df)
+
+    print('Fitting model...')
     recommender = sr.fit(data)
+
     print('Getting predictions...')
     preds = sr.get_predictions_for_all_users(df, recommender, n_predictions=10)
+    
     print('Writing predictions...')
     db.write_predictions(preds, 'predictions_temp')
 
