eda0041f68e0e11118a7f2330bd7e22c01330891
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2018-12-14 14:43:08 -0800

    add comments

--- mixlab/spotify_scraper.py
+++ mixlab/spotify_scraper.py
@@ -25,6 +25,17 @@ class SpotifyScraper():
 
 
     def get_artist_uri(self, artist_name):
+        """Search artist by name and return uri exact match or most popular match
+        
+        Parameters
+        ----------
+        artist_name: string, name of artist
+
+        Returns
+        -------
+        uri : string
+        
+        """
         artist_uri = []
         results = self.spotify.search(artist_name, type='artist')
         for item in results['artists']['items']:
@@ -38,6 +49,15 @@ class SpotifyScraper():
             return self._choose_most_popular_artist(results)
 
     def get_artist_albums(self, artist_uri):
+        """Get all album uris for an artist
+        Parameters
+        ----------
+        artist_uri: string
+
+        Returns
+        -------
+        albums : list of strings
+        """
         results = self.spotify.artist_albums(artist_id=artist_uri)
         albums = results['items']
         while results['next']:
@@ -47,14 +67,34 @@ class SpotifyScraper():
       
     
     def get_album_tracks(self, album_uri):
+        """Get all track uris for an album
+        Parameters
+        ----------
+        album_uri: list of strings
+
+        Returns
+        -------
+        tracks : list of strings
+        """
         results = self.spotify.album_tracks(album_uri)
         tracks = results['items']
         while results['next']:
             results = self.spotify.next(results)
             tracks.extend(results['items'])
         return [track['uri'] for track in tracks]
+
     
     def search_artist_get_tracks(self, artist_name):
+        """Search an artist by name and return all track uris as a list
+        Parameters
+        ----------
+        album_uri: list of strings
+
+        Returns
+        -------
+        tracks : list of strings
+        """
+
         artist_uri = self.get_artist_uri(artist_name)
         album_uris = self.get_artist_albums(artist_uri)
 
@@ -65,6 +105,15 @@ class SpotifyScraper():
         return tracks
 
     def _choose_most_popular_artist(self, search_results):
+        """From list of artist search results, return most pupular
+        Parameters
+        ----------
+        search_results : output of spotify.search('artist_name')
+
+        Returns
+        -------
+        artist_item : dictionary
+        """
         results = search_results['artists']
         artists = np.chararray(results['total'], itemsize=50)
         popularity = np.zeros(results['total'])
@@ -74,7 +123,16 @@ class SpotifyScraper():
         return artists[popularity.argmax()].decode('utf-8')
     
     def get_song_features(self, song_list, song_titles=None, song_features=None):
+        """From list of artist search results, return most pupular
+        Parameters
+        ----------
+        song_list : song uris as a list of strings
 
+        Returns
+        -------
+        song_titles : song titles as list of strings
+        song_features : list of dictionaries, one dictionary for each song
+        """
         if song_titles is None:
             song_features = {}
             song_titles = {}
@@ -102,7 +160,7 @@ class SpotifyScraper():
                 song_titles.update({song_list[i]: track['name']}) 
             # song_titles.extend(temp) 
             song_list = song_list[50:].copy()
-            return self.get_song_names(song_list, song_titles, song_features)
+            return self.get_song_features(song_list, song_titles, song_features)
 
             
 
