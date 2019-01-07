0191349660757b9dcc53527f8e2d7950ef0053b7
Brett Ashley McCoubrey <brett.mccoubrey@gmail.com>
2019-01-06 21:08:23 -0800

    remove commented code

--- mixlab/website/app.py
+++ mixlab/website/app.py
@@ -4,9 +4,6 @@ from flask import Flask, request, render_template, jsonify
 from ..web_functions import WebFunctionHandler
 
 
-# with open('.pkl') as f:
-#     model = pickle.load(f)
-
 app = Flask(__name__, static_url_path="")
 
 wfh = WebFunctionHandler()
@@ -18,7 +15,6 @@ def index():
     return render_template('index.html',
                                 artists_selection=artists_selection)
 
-
 @app.route('/get_selector_for_songs/<int:artist_id>')
 def get_selector_for_songs(artist_id):
     artist_songs = wfh.get_selector_for_songs(artist_id)
@@ -28,13 +24,3 @@ def get_selector_for_songs(artist_id):
 def get_predictions(song_id):
     data = wfh.get_predictions(song_id)
     return render_template('recommender_table.html', data=data)
-
-
-# @app.route('/artist/<int:artist_id>')
-# def artist_songs(artist_id):
-#     """Return songs for an artist."""
-#     artist_songs = wfh.get_selector_for_songs(artist_id)
-#     results = wfh.get_predictions(song_id)
-#     return render_template('index.html',
-#                                 artist_songs=artist_songs,
-#                                 results=results)
